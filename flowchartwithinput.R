004442e758e7c5b433faf4b8a6968554325db76f
DaniekD <64411718+DaniekD@users.noreply.github.com>
2020-05-12 14:52:37 +0200

    Update flowchartwithinput.R

--- flowchartwithinput.R
+++ flowchartwithinput.R
@@ -30,12 +30,12 @@ shinyApp(
       user_list_inp = c(subs$user)
       user_list_unique_inp <- unique(user_list_inp)
       
-      startmatrix <- matrix(nrow = 6, ncol = 6)
-      startmatrix[] <- 0L
+      startmatrix_inp <- matrix(nrow = 6, ncol = 6)
+      startmatrix_inp[] <- 0L
       for (n in user_list_unique_inp) {
         for_seq <- c(subset(subs, user == n)$Cluster)
         sequenceMatr_for <-createSequenceMatrix(for_seq,sanitize=FALSE, possibleStates = seq(1:6))
-        startmatrix_inp <- startmatrix + sequenceMatr_for
+        startmatrix_inp <- startmatrix_inp + sequenceMatr_for
       }
       plot <- plotmat(A = as.data.frame(startmatrix_inp), pos = 6, curve = 0.7, name = name, lwd = 2, arr.len = 0.6, arr.width = 0.25, my = -0.2, box.size = 0.05, arr.type = "triangle", dtext = 0.95, main = "Flow chart AOI")
     })
